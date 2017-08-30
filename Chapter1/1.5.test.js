1657c5fe2e02bb4e45aabd06f8e235a31ca68fd8
mynameislich <zuoyuan-zhao@uiowa.edu>
2017-08-29 21:02:11 -0500

    ch1 question 4 and question 5 (#12)
    * ch1 question 4 and question 5
    
    * indentation problem solved

---
+++ Chapter1/1.5.test.js
@@ -0,0 +1,25 @@
+const oneAway = require('./1.5');
+
+describe('testing oneAway(string)', () => {
+  test('empty strings should return true', () => {
+    expect(oneAway('',"")).toBe(true);
+  });
+
+  test('"pale","ple" should return true', () => {
+    expect(oneAway('',"")).toBe(true);
+  });
+
+  test('"abcde" should return true', () => {
+    expect(oneAway("pale","ple" )).toBe(true);
+  });
+
+  test('"pales","pale"  should return true', () => {
+    expect(oneAway("pales","pale" )).toBe(true);
+  });
+  test('"pale","bale"  should return true', () => {
+    expect(oneAway("pale","bale" )).toBe(true);
+  });
+  test('"pale","bake"  should return false', () => {
+    expect(oneAway("pale","bake" )).toBe(false);
+  });
+});
